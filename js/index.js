1d50f2592a130de23fdf2f007a2764b32a99e910
jihong.zjh <jihong.zjh@alibaba-inc.com>
2015-12-25 09:39:52 +0800

    背景随天气变化

--- js/index.js
+++ js/index.js
@@ -12,9 +12,19 @@ function myFun(result){
       var weather_data = result.weather_data;
       
       //城市、风力
+	  var weather_today = result.weather_data[0].weather;
       $("#location").html(result.currentCity);
-      $("#weather").html(result.weather_data[0].weather);
+      $("#weather").html(weather_today);
       $("#wind").html(result.weather_data[0].wind);
+
+	  //根据天气替换背景
+	  if(/雨/.test(weather_today)){
+          $("body").css({"background-image":"url('http://ww1.sinaimg.cn/mw690/62d95157gw1ezb4l2hvg9j211y0lce5s.jpg')"});
+      }else if(/雪/.test(weather_today)){
+		  $("body").css({"background-image":"url('http://ww1.sinaimg.cn/mw690/62d95157gw1ezb4wzmvmtj211y0lchdt.jpg')"});
+	  }else if(/多云|阴/.test(weather_today)){
+          $("body").css({"background-image":"url('http://ww1.sinaimg.cn/mw690/62d95157gw1ezb4kyvuzmj211y0lcahf.jpg')"});
+      }
       
       //天气情况
       for(var i in weather_data){
@@ -27,3 +37,10 @@ function myFun(result){
 
 var myCity = new BMap.LocalCity();
 myCity.get(myFun);
+
+var width = window.screen.availWidth;
+var height = window.screen.availHeight;
+if(height > width){
+  //手机屏幕适配
+  $(".container").css({"margin-top": "40%"});
+}
